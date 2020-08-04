6ab9e8ce9841ea0c15869b5d864780255eb4a274
Renzo Cammi <62706597+rcammi@users.noreply.github.com>
2020-08-04 15:21:40 -0300

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,6 +1,6 @@
 import unidecode
 
-path = "C:/Users/Renzo/Documents/Ren/Proyectos/futbol_empirico/analisis_ligas/Superliga/DefAttributesSuperliga/"
+path = ""
 
 with open(path + "whoscored_data.txt", "r", encoding="utf-8") as f:
     data_list = f.readlines()
